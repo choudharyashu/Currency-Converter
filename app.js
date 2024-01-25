e9a8635562f46971f428a23319084145aab0813c
ASHU <choudhary6802@gmail.com>
2024-01-25 23:29:24 +0530

    Update

---
+++ app.js
@@ -0,0 +1,14 @@
+const URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+const dropdown = document.querySelector(".dropdown select");
+
+for (select of dropdown)
+{
+    console.log(select);
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value=currCode;
+        select.append(newOption);
+    }
+}
