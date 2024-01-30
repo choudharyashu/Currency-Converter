6097d17e34f079d1eb4e69e4afdcca4e65a3c146
ASHU <choudhary6802@gmail.com>
2024-01-30 22:16:25 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
 
-const dropdown = document.querySelector(".dropdown select");
+const dropdown = document.querySelectorAll(".dropdown select");
 
 for (select of dropdown)
 {
