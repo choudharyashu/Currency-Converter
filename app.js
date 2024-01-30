4ddbb876d56dfc63512bda5aecfbddfe9544ab22
ASHU <choudhary6802@gmail.com>
2024-01-31 01:26:51 +0530

    final

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+const baseURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
 
 const dropdown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 for (select of dropdown)
 {
@@ -9,6 +13,44 @@ for (select of dropdown)
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value=currCode;
+        if (select.name=="from" && currCode=="USD")
+        {
+            newOption.selected="selected";
+        }
+        else if (select.name=="to" && currCode=="INR")
+        {
+            newOption.selected="selected";
+        }
         select.append(newOption);
+        
+        select.addEventListener("change",(evt)=>{
+            updateFlag(evt.target);
+        });
     }
 }
+
+const updateFlag = (element)=>{
+    let currCode=element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt)=>{
+    evt.preventDefault();    
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal=="" || amtVal<0){
+        amtVal=0;
+        amount.value="0";
+    }
+
+    const URL = `${baseURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data =  await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amtVal*rate;
+
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+});
